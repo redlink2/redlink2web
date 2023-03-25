25b9f9b5f89f9aae126a9913411be32a9e538cf9
Ashton Redd <redlink2@pm.me>
2023-03-25 03:04:58 -0500

    added comments with ideas

--- pages/games.vue
+++ pages/games.vue
@@ -3,9 +3,15 @@
 		<h1>Welcome to my website</h1>
 		<ul>
 			<li><nuxt-link to="/">Home</nuxt-link></li>
+			<!-- tic-tac-toe -->
+			<li><a href="#">Coming Soon</a></li>
+			<!-- blackjack -->
 			<li><nuxt-link to="/blackjack">Play Blackjack</nuxt-link></li>
+			<!-- simon -->
 			<li><a href="#">Coming Soon</a></li>
+			<!-- minesweeper -->
 			<li><a href="#">Coming Soon</a></li>
+			<!-- hangman -->
 			<li><a href="#">Coming Soon</a></li>
 		</ul>
 	</div>
