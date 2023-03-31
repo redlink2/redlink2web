bfa95af33b44aa895c826d85406ea09baaa714fd
Ashton Redd <redlink2@pm.me>
2023-03-30 22:48:12 -0500

    fixed favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,15 @@ export default defineNuxtConfig({
 		port: 3000,
 		host: '0.0.0.0',
 	},
-
+	head: {
+		title: '~Nostalgia~',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' },
+			{ name: 'format-detection', content: 'telephone=no' },
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
+	components: true,
 })
