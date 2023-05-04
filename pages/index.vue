08e3f2f0863e062624ca8b85d78d4a0741b45364
Ashton Redd <redlink2@pm.me>
2023-05-04 03:32:57 -0500

    refactor and redesign index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,100 @@
 <template>
-	<title>Playground</title>
-	<header class="navbar">
-		<Navbar />
-	</header>
-	<main>
-		<div class="container">
-			<div
-				v-for="(section, index) in sections"
-				:key="index"
-				class="thumbnail"
-				@click="goToSection(section.path)"
-				tabindex="0"
-				role="button"
-				:aria-label="section.name"
-			>
-				<img :src="section.image" :alt="section.name" class="thumbnail-image" />
-				<div class="thumbnail-hover">
-					<p>{{ section.description }}</p>
+	<div>
+		<main>
+			<div class="container">
+				<div
+					v-for="(section, index) in sections"
+					:key="index"
+					class="thumbnail"
+					@click="goToSection(section.path)"
+					tabindex="0"
+					role="button"
+					:aria-label="section.name"
+				>
+					<img
+						:src="section.image"
+						:alt="section.name"
+						class="thumbnail-image"
+					/>
+					<div class="thumbnail-hover">
+						<p>{{ section.description }}</p>
+					</div>
 				</div>
 			</div>
-		</div>
-	</main>
+		</main>
+	</div>
 </template>
 
 <script>
-	import Navbar from '~/components/Navbar.vue';
-
 	export default {
 		data() {
 			return {
-				isLoading: true,
 				sections: [
-					{ name: 'Games', path: '/games/games', image: '/pages/gallery/thumbs/shapes-in-red.png', description: 'Play fun 90s games!' },
-					{ name: 'Art Gallery', path: '/gallery', image: '/pages/gallery/thumbs/weird dude.png', description: 'Explore amazing artwork.' },
-					{ name: 'Blog', path: '/blog', image: '/pages/gallery/thumbs/weirdo.png', description: 'Read about interesting topics.' },
-					{ name: 'Startpage', path: '/startpager', image: '/pages/gallery/thumbs/start.png', description: 'My personalized startpage.' },
-					{ name: 'Links', path: '/links', image: '/pages/gallery/thumbs/OntoSomethingNewCurtain.png', description: 'Discover other cool websites.' },
+					{
+						name: "Games",
+						path: "/games/games",
+						image: "/pages/gallery/thumbs/shapes-in-red.png",
+						description: "Play a game!",
+					},
+					{
+						name: "Art Gallery",
+						path: "/gallery",
+						image: "/pages/gallery/thumbs/weird dude.png",
+						description: "View some art!",
+					},
+					{
+						name: "Blog",
+						path: "/blog",
+						image: "/pages/gallery/thumbs/weirdo.png",
+						description: "Read my blog!",
+					},
+					{
+						name: "Dummy",
+						path: "/",
+						image: "/pages/gallery/thumbs/OntoSomethingNewCurtain.png",
+						description: "Test!",
+					},
+					{
+						name: "Startpage",
+						path: "/startpager",
+						image: "/pages/gallery/thumbs/start.png",
+						description: "My startpage!",
+					},
+					{
+						name: "Collection",
+						path: "/collection",
+						image: "/pages/gallery/thumbs/OntoSomethingNewCurtain.png",
+						description: "Visit your collection!",
+					},
 				],
 			};
 		},
-		async fetch() {
-			document.body.classList.add('main');
-		},
-		beforeUnmount() {
-			document.body.classList.remove('main');
-		},
 		methods: {
 			goToSection(path) {
-			this.$router.push(path);
+				this.$router.push(path);
 			},
 		},
 	};
 </script>
 
-
 <style scoped>
 	html {
 		scroll-behavior: smooth;
-		cursor: url('/images/custom-cursor.png'), auto;
+		cursor: url("/images/custom-cursor.png"), auto;
 	}
 
 	main {
-		min-height: 80vh;
-		margin: -.9vh;
-		padding: 10vh;
-		background-image: url('/pages/nostalgia.png');
+		background-image: url("/pages/nostalgia.png");
 		background-repeat: repeat;
 	}
 
 	.container {
 		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-		gap: 5vh;
-		padding-top: 8vh;
+		grid-template-columns: repeat(auto-fit, minmax(30vw, 1fr));
+		gap: 2vw;
+		padding: 2vw;
 	}
+
 
 	/* Thumbnails */
 	.thumbnail {
@@ -97,6 +118,8 @@
 
 	.thumbnail-hover {
 		position: absolute;
+		top: 0;
+		left: 0;
 		width: 100%;
 		height: 100%;
 		background-color: rgba(255, 0, 0, 0.5);
@@ -106,35 +129,23 @@
 		flex-direction: column;
 		justify-content: flex-end;
 		align-items: center;
-		padding-bottom: 20vh;
 	}
 
-	.thumbnail {
-		display: flex;
-		align-items: center;
-		position: relative;
-		cursor: pointer;
-		overflow: hidden;
-	}
 	.thumbnail:hover .thumbnail-hover {
 		opacity: 1;
 	}
 
 	/* Responsive Design */
 	@media screen and (max-width: 768px) {
-		main {
+		.container {
 			background-size: 16vh;
 		}
 
 		.container {
-			grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+			grid-template-columns: repeat(auto-fit, minmax(15vw, 1fr));
 			gap: 3vh;
 			padding-top: 3vh;
 			padding-left: 5vw;
-		}
-
-		.nav-item {
-			margin: 0 0.5rem;
 		}
 
 		.thumbnail {
@@ -146,4 +157,5 @@
 			padding-bottom: 5vh;
 		}
 	}
+
 </style>
