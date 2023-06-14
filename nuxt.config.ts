4556ab48851c09374264c5112b0c60f66306d87b
Ashton Redd <redlink2@pm.me>
2023-06-14 01:44:41 -0500

    adding routes | formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	router: {
+		routes: [
+			{
+				path: "/gallery",
+				component: "~/pages/gallery.vue",
+			},
+		],
+	},
 	server: {
 		port: 3000,
-		host: '0.0.0.0',
+		host: "0.0.0.0",
 	},
 	head: {
-		title: '~Nostalgia~',
+		title: "~Nostalgia~",
 		meta: [
-			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: '' },
-			{ name: 'format-detection', content: 'telephone=no' },
+			{ charset: "utf-8" },
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{ hid: "description", name: "description", content: "" },
+			{ name: "format-detection", content: "telephone=no" },
 		],
-		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
 	},
 	components: true,
 	env: {
 		FAUNADB_KEY: process.env.FAUNADB_KEY,
 	},
-})
+});
