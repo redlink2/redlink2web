7a96129881624facbf9daca174857c825cacb0e7
Ashton Redd <redlink@redlink2.com>
2023-07-02 00:48:44 -0500

    Squashed commit of the following:
    commit 6931c190d16df64870dbeb7e664cfda90810abdc
    Author: Ashton Redd <redlink@redlink2.com>
    Date:   Sun Jul 2 00:34:17 2023 -0500
    
        implemented basic modal for images
    
    commit cb7fd1df2c3ebf618fefdd9f11e707af2dabd818
    Author: Ashton Redd <redlink@redlink2.com>
    Date:   Sun Jul 2 00:33:54 2023 -0500
    
        added NuxtLabsUI to package.json
    
    commit b62c12d621ece052933793a32205b240c2572004
    Author: Ashton Redd <redlink@redlink2.com>
    Date:   Sun Jul 2 00:33:23 2023 -0500
    
        removed remains of vue-masonry-wall
    
    commit 1a0856f6e17a667a6fcc0a329d857de4e53cc685
    Author: Ashton Redd <redlink@redlink2.com>
    Date:   Sat Jul 1 00:39:54 2023 -0500
    
        sort by aspect ratio
    
    commit eee1f1e90cd386c602c4fdebf8878a46e2539d96
    Author: Ashton Redd <redlink@redlink2.com>
    Date:   Fri Jun 30 22:27:14 2023 -0500
    
        removed vue-masonry-wall for purecss
    
    commit e81719756e2bbb35f5409a40eb415886cfcf948a
    Author: Ashton Redd <redlink@redlink2.com>
    Date:   Sun Jun 25 03:47:07 2023 -0500
    
        minor changes
    
    commit 0e17e967487c7a73297f5b8be4540b77fee7e3d9
    Author: Ashton Redd <redlink@redlink2.com>
    Date:   Sat Jun 24 14:04:03 2023 -0500
    
        fixed columns css
    
    commit 382fab5403e6811726b4379429b863e51e7d632e
    Author: Ashton Redd <redlink@redlink2.com>
    Date:   Sat Jun 24 01:17:45 2023 -0500
    
        added updateLayout and more

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,26 +1,35 @@
 <template>
 	<div class="container">
-		<MasonryWall
-			:items="images"
-			:ssr-columns="1"
-			:column-width="500"
-			:gap="32"
-		>
-			<template #default="{ item, index }">
-				<GalleryImages :image="item" v-if="images.length" />
-			</template>
-		</MasonryWall>
+		<div v-for="(image, index) in images" :key="index" class="grid-item">
+			<div class="thumbnail" @click="openModal(image)">
+				<img
+					:src="image.path"
+					:alt="image.name"
+					class="thumbnail-image"
+				/>
+				<div class="thumbnail-hover">
+					<p>{{ image.name }}</p>
+				</div>
+			</div>
+		</div>
+		<UModal v-model="showModal">
+			<img :src="currentImage.path" :alt="currentImage.name" />
+		</UModal>
 	</div>
 </template>
 
 <script setup>
 	import { ref, onMounted } from "vue";
 	import { Client, query as q } from "faunadb";
-	// import gallery images component
-	import GalleryImages from "../components/GalleryImages.vue";
-	import MasonryWall from "@yeger/vue-masonry-wall";
 
 	let images = ref([]);
+	let showModal = ref(false);
+	let currentImage = ref({});
+
+	const openModal = (image) => {
+		currentImage.value = image;
+		showModal.value = true;
+	};
 
 	onMounted(async () => {
 		const client = new Client({ secret: import.meta.env.VITE_FAUNADB_KEY });
@@ -31,11 +40,17 @@
 					q.Lambda((x) => q.Get(x))
 				)
 			);
-			images.value = result.data.map((item) => item.data);
-			console.log(
-				"Fetched images:",
-				result.data.map((item) => item.data)
-			);
+
+			// Sort the images by aspect ratio
+			images.value = result.data
+				.map((item) => {
+					const image = item.data;
+					image.aspectRatio = image.width / image.height;
+					return image;
+				})
+				.sort((a, b) => a.aspectRatio - b.aspectRatio);
+
+			console.log("Fetched and sorted images:", images.value);
 		} catch (error) {
 			console.error("Error fetching images:", error);
 		}
@@ -45,15 +60,67 @@
 <style scoped>
 	html {
 		scroll-behavior: smooth;
-	}
-
-	main {
-		background-color: purple;
 		background-repeat: repeat;
 	}
 
 	.container {
-		padding: 3vh;
 		background-color: purple;
+		display: grid;
+		grid-template-columns: repeat(5, 1fr);
+		grid-auto-rows: 20vh;
+		grid-gap: 2vw;
+	}
+
+	.grid-item {
+		border: 2px solid black;
+		text-align: center;
+		position: relative;
+		cursor: pointer;
+		overflow: hidden;
+	}
+
+	.thumbnail-image {
+		width: 100%;
+		height: 100%;
+		transition: transform 0.3s ease;
+		object-fit: cover;
+	}
+
+	.thumbnail:hover .thumbnail-image {
+		transform: scale(1.1);
+	}
+
+	.thumbnail-hover {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(255, 0, 0, 0.5);
+		opacity: 0;
+		transition: opacity 0.3s ease;
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
+		align-items: center;
+	}
+
+	.thumbnail:hover .thumbnail-hover {
+		opacity: 1;
+	}
+
+	@media (max-width: 50vw) {
+		.container {
+			grid-template-columns: repeat(3, 1fr);
+		}
+
+		.grid-item {
+			height: 25vh;
+			width: 50vw;
+		}
+
+		.thumbnail-hover {
+			padding-bottom: 5vh;
+		}
 	}
 </style>
