0fa66a01bdf5cbd5c977f93138c090158654cb45
Ashton Redd <redlink@redlink2.com>
2023-07-10 04:19:23 -0500

    minor css changes

--- pages/playground.vue
+++ pages/playground.vue
@@ -4,15 +4,15 @@
 		<h1>Play a game!</h1>
 		<ul>
 			<!-- tic-tac-toe -->
-			<li><nuxt-link href="/games/tictactoe">Tic-Tac-Toe</nuxt-link></li>
-			<!-- blackjack -->
-			<li><nuxt-link to="/games/blackjack">Blackjack</nuxt-link></li>
-			<!-- simon -->
-			<li><nuxt-link href="/games/simon">Simon</nuxt-link></li>
+			<li><nuxt-link to="/games/tictactoe">Tic-Tac-Toe</nuxt-link></li>
 			<!-- minesweeper -->
 			<li><a href="#" class="tba">Coming Soon</a></li>
 			<!-- hangman -->
 			<li><a href="#" class="tba">Coming Soon</a></li>
+			<!-- simon -->
+			<li><nuxt-link class="wip" to="/games/simon">Simon (WIP)</nuxt-link></li>
+			<!-- blackjack -->
+			<li><nuxt-link class="wip" to="/games/blackjack">Blackjack (WIP)</nuxt-link></li>
 			<!-- home -->
 			<li><nuxt-link to="/">Home</nuxt-link></li>
 		</ul>
@@ -36,5 +36,9 @@
 	}
 	a.tba {
 		color: yellow;
+	}
+
+	a.wip {
+		color: chartreuse;
 	}
 </style>
